4d48cbf8c9d54be49cdfc34a5acb8a3706a1b707
frumsy <liambritt3@gmail.com>
2020-11-13 16:34:13 -0500

    selectable hsk level

--- src/components/Vocab/index.js
+++ src/components/Vocab/index.js
@@ -1,6 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DataGrid } from '@material-ui/data-grid';
 import { Link } from "react-router-dom";
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+// import { makeStyles } from '@material-ui/core/styles';
+
+// const useStyles = makeStyles((theme) => ({
+//   container: {
+//     width: "auto",
+//     marginLeft: "0px",
+//     marginRight: "0px",
+//   },
+// }));
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
@@ -9,15 +20,39 @@ const columns = [
   { field: 'def', headerName: 'def', type: 'number', width: 700 },
 ];
 
-const handleChooseLevel = (e)=>{
-  
-}
 export default ({hsks}) => {
-  const [level, setLevel] = useState(hsks[1])
+  console.log(hsks)
+  const [level, setLevel] = useState(0)
+  const [currentHSK, setCurrentHSK] = useState(hsks[level])
+  // const classes = useStyles()
+
+  useEffect(()=>{
+    setCurrentHSK(hsks[level])
+  },[level, hsks])
+
+  const handleChange = (e)=>{
+    console.log("value",e.target.value)
+    setLevel(e.target.value);
+  }
+  
   return (
     <div style={{ height: 400, width: '100%' }}>
       <Link to="/">practice</Link>
-      <DataGrid rows={level} columns={columns} pageSize={5} checkboxSelection />
+      <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={level}
+          onChange={handleChange}
+        >
+          <MenuItem value={1-1}><em>HSK1</em></MenuItem>
+          <MenuItem value={2-1}>HSK2</MenuItem>
+          <MenuItem value={3-1}>HSK3</MenuItem>
+          <MenuItem value={4-1}>HSK4</MenuItem>
+          <MenuItem value={5-1}>HSK5</MenuItem>
+          <MenuItem value={6-1}>HSK6</MenuItem>
+        </Select>
+        <DataGrid rows={currentHSK} columns={columns} pageSize={5} checkboxSelection />
+
     </div>
   );
 }
